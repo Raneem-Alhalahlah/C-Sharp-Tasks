d6cb0316cd3fc50b185e89723a802658635ced25
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-07-04 10:11:38 +0300

    C#-ASP-Task2

---
+++ task 2 asp/task 2 asp/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="task_2_asp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+       <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" Height="16px" OnTextChanged="TextBox1_TextChanged">Result:</asp:TextBox> <br /><br />
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Number 1"></asp:Label>
+           <asp:TextBox ID="Num1" runat="server" Text=""></asp:TextBox><br /> <br />
+            <asp:Label ID="Label2" runat="server" Text="Number2"></asp:Label>
+            <asp:TextBox ID="Num2" runat="server" Text=""></asp:TextBox><br /><br /> <br />
+        <asp:Button ID="Button1" runat="server" Height="58px" OnClick="Button1_Click" Text="+" Width="137px" />
+        <asp:Button ID="Button2" runat="server" Height="58px" OnClick="Button2_Click" style="margin-top: 0px" Text="-" Width="137px" />
+        <asp:Button ID="Button3" runat="server" Height="58px" OnClick="Button3_Click" Text="*" Width="137px" />
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ task 2 asp/task 2 asp/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task_2_asp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(Num1.Text);
+            int num2 = int.Parse(Num2.Text);
+            int result = num1 * num2;
+            TextBox1.Text = $" The result is {result.ToString()}";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(Num1.Text);
+            int num2 = int.Parse(Num2.Text);
+            int result = num1 + num2;
+            TextBox1.Text = $" The result is {result.ToString()}";
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(Num1.Text);
+            int num2 = int.Parse(Num2.Text);
+            int result = num1 - num2;
+            TextBox1.Text = $" The result is {result.ToString()}";
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
+
+    
